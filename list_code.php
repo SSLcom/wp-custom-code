ac59c665d9a9d0b5d37aeecf454443dc090c7d8c
Aaron Dancer <me@aarondancer.com>
2015-05-28 17:27:08 -0500

    List code in directories

--- list_code.php
+++ list_code.php
@@ -1,3 +1,21 @@
-<?php
-	echo $active_tab;
-?>
+<table style="width: 100%">
+	<thead>
+		<th style="text-align: left;">File name</th>
+		<th style="text-align: right;">Edit Options</th>
+	</thead>
+	<tbody>
+		<?php
+			if ($handle = opendir(dirname(__FILE__) . '\\'  . $active_tab)) {
+			    while (false !== ($entry = readdir($handle))) {
+			        if ($entry != "." && $entry != "..") {
+			        	echo "<tr>";
+			            echo "<td style='width:80%'>$entry</td>";
+			            echo '<td style="text-align: right;"><a href="">Edit</a> <a href="">Delete</a><td>';
+			            echo "</tr>";
+			        }
+			    }
+			    closedir($handle);
+			}
+		?>
+	</tbody>
+</table>
