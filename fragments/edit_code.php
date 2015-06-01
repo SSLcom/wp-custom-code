20e2e56e4a51699f982709f1bcf68771c311590e
Aaron Dancer <me@aarondancer.com>
2015-06-01 12:02:32 -0500

    Added comments

--- fragments/edit_code.php
+++ fragments/edit_code.php
@@ -19,13 +19,16 @@
 			die();
 		}    
 	}
+	//Write code to file
 	if (isset($_POST["title"])) {
 		$writeFile = dirname(dirname(__FILE__)) . "\\" . $active_tab . "\\" . $_POST["title"];
 		$code = stripslashes($_POST["code"]);
 		file_put_contents($writeFile, $code);
+		//Redirect to edit inputted file name
 		if ($_GET["edit"] !== "") redirect(str_replace($_GET["edit"], $_POST["title"], getCurrentURL()));
 		else redirect(getCurrentURL() . "=" . $_POST["title"]);
 	}
+	//Load code to screen
 	$codeFile = dirname(dirname(__FILE__)) . "\\" . $active_tab . "\\" . $_GET["edit"];
 	echo '<form method="post" action="">';
 	echo '<input type="text" name="title" size="30" id="title" spellcheck="true" autocomplete="off" value="'. (($_GET["edit"] !== "") ? $_GET["edit"] : "") . '">';
