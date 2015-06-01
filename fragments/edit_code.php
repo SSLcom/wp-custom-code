2479579137f8f0c52012f06bd07bd49262c846e3
Aaron Dancer <me@aarondancer.com>
2015-06-01 12:37:46 -0500

    Added file name validation

--- fragments/edit_code.php
+++ fragments/edit_code.php
@@ -1,7 +1,6 @@
 <br>
 <?php
-	function getCurrentURL()
-	{
+	function getCurrentURL() {
 	    $currentURL = (@$_SERVER["HTTPS"] == "on") ? "https://" : "http://";
 	    $currentURL .= $_SERVER["SERVER_NAME"];
 	 
@@ -11,7 +10,7 @@
         $currentURL .= $_SERVER["REQUEST_URI"];
 	    return $currentURL;
 	}
-	function redirect($url){
+	function redirect($url) {
 		if (headers_sent()){
 			die('<script type="text/javascript">window.location.href="' . $url . '";</script>');
 		}else{
@@ -21,20 +20,25 @@
 	}
 	//Write code to file
 	if (isset($_POST["title"])) {
-		$writeFile = dirname(dirname(__FILE__)) . "\\" . $active_tab . "\\" . $_POST["title"];
 		$code = stripslashes($_POST["code"]);
-		file_put_contents($writeFile, $code);
-		//Redirect to edit inputted file name
-		if ($_GET["edit"] !== "") redirect(str_replace($_GET["edit"], $_POST["title"], getCurrentURL()));
-		else redirect(getCurrentURL() . "=" . $_POST["title"]);
+		//Check to see if file name is valid
+		if ($_POST["title"] !== "" && !preg_match("/[^0-9A-Z.\-_]/i", $_POST["title"])) {
+			$writeFile = dirname(dirname(__FILE__)) . "\\" . $active_tab . "\\" . $_POST["title"];
+			file_put_contents($writeFile, $code);
+			//Redirect to edit inputted file name
+			if ($_GET["edit"] !== "") redirect(str_replace($_GET["edit"], $_POST["title"], getCurrentURL()));
+			else redirect(getCurrentURL() . "=" . $_POST["title"]);
+		}
+		else echo "File name is invalid";
 	}
 	//Load code to screen
 	$codeFile = dirname(dirname(__FILE__)) . "\\" . $active_tab . "\\" . $_GET["edit"];
 	echo '<form method="post" action="">';
 	echo '<input type="text" name="title" size="30" id="title" spellcheck="true" autocomplete="off" placeholder="Enter file name here" value="'. (($_GET["edit"] !== "") ? $_GET["edit"] : "") . '">';
+	if ($_GET["edit"] !== "" && !isset($code)) $code = file_get_contents($codeFile);
 ?>
 <br><br>
-<textarea name="code" rows="10" cols="30" placeholder="Enter code here"><?php if ($_GET["edit"] !== "") echo file_get_contents($codeFile); ?></textarea>
+<textarea name="code" rows="10" cols="30" placeholder="Enter code here"><?php echo $code ?></textarea>
 <br>
 <button class="button button-primary button-large">Save .<?php echo $active_tab ?> file</button>
 </form>
