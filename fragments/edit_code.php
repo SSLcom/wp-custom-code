48e32a4489733326cba29b0373b1b9f79e31ac74
Aaron Dancer <me@aarondancer.com>
2015-06-01 00:36:27 -0500

    Save and redirect
    Files will now save without the need of external actions. If the file saved has a different name from the original, it will redirect to the new file when saved.

--- fragments/edit_code.php
+++ fragments/edit_code.php
@@ -1,9 +1,32 @@
 <br>
 <?php
+	function getCurrentURL()
+	{
+	    $currentURL = (@$_SERVER["HTTPS"] == "on") ? "https://" : "http://";
+	    $currentURL .= $_SERVER["SERVER_NAME"];
+	 
+	    if($_SERVER["SERVER_PORT"] != "80" && $_SERVER["SERVER_PORT"] != "443")
+	        $currentURL .= ":".$_SERVER["SERVER_PORT"];
+	 
+        $currentURL .= $_SERVER["REQUEST_URI"];
+	    return $currentURL;
+	}
+	function redirect($url){
+		if (headers_sent()){
+			die('<script type="text/javascript">window.location.href="' . $url . '";</script>');
+		}else{
+			header('Location: ' . $url);
+			die();
+		}    
+	}
+	if (isset($_POST["title"])) {
+		$writeFile = dirname(dirname(__FILE__)) . "\\" . $active_tab . "\\" . $_POST["title"];
+		file_put_contents($writeFile, $_POST["code"]);
+		redirect(str_replace($_GET["edit"], $_POST["title"], getCurrentURL()));
+	}
 	$codeFile = dirname(dirname(__FILE__)) . "\\" . $active_tab . "\\" . $_GET["edit"];
-	echo '<form action="' . get_site_url() . '/wp-content/plugins/sslcom-custom-code/actions/save_code.php">';
+	echo '<form method="post" action="">';
 	echo '<input type="text" name="title" size="30" id="title" spellcheck="true" autocomplete="off" value="'. $_GET["edit"] . '">';
-	echo '<input name="type" value="' . $active_tab . '" style="display:none">'
 ?>
 <br><br>
 <textarea name="code" rows="10" cols="30"><?php echo file_get_contents($codeFile); ?></textarea>
