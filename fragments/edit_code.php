802144bdb8e3bdeb8299e017caf4e70a1b791103
Aaron Dancer <me@aarondancer.com>
2015-06-01 00:05:06 -0500

    Improved readability

--- fragments/edit_code.php
+++ fragments/edit_code.php
@@ -1,12 +1,12 @@
 <br>
-<a class="button button-primary button-large">Save .<?php echo $active_tab ?> file</a>
-<br><br>
-<?php echo '<input type="text" name="title" size="30" id="title" spellcheck="true" autocomplete="off" value="'. $_GET["edit"] . '">' ?>
-<br><br>
 <?php
 	$codeFile = dirname(dirname(__FILE__)) . "\\" . $active_tab . "\\" . $_GET["edit"];
-	$fh = fopen($codeFile, 'r+');
-	$theData = fread($fh, filesize($codeFile));
-	fclose($fh);
+	echo '<form action="' . get_site_url() . '/wp-content/plugins/sslcom-custom-code/actions/save_code.php">';
+	echo '<input type="text" name="title" size="30" id="title" spellcheck="true" autocomplete="off" value="'. $_GET["edit"] . '">';
+	echo '<input name="type" value="' . $active_tab . '" style="display:none">'
 ?>
-<textarea rows="10" cols="30"><?php echo $theData ?></textarea>
+<br><br>
+<textarea name="code" rows="10" cols="30"><?php echo file_get_contents($codeFile); ?></textarea>
+<br>
+<button class="button button-primary button-large">Save .<?php echo $active_tab ?> file</button>
+</form>
