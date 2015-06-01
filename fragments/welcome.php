2c6c8bd0e9abd4e12ec1a2d9419c617ea818453d
Aaron Dancer <me@aarondancer.com>
2015-06-01 12:57:04 -0500

    Added content to welcome

---
+++ fragments/welcome.php
@@ -0,0 +1,10 @@
+<h2>Welcome to the SSL.com Custom Code Plugin file manager for Wordpress!</h2>
+<h3>What is the SSL.com Custom Code Plugin?</h3>
+<p>The SSL.com Custom Code Plugin allows you to insert your own custom HTML, PHP, CSS, and JS into wordpress via easy to use shortcodes. You can add your custom code in the tabs above.</p>
+<h3>Examples of how to insert custom code</h3>
+<p>
+	[html name=file.html]<br>
+	[php name=file.php]<br>
+	[css name=file.css]<br>
+	[js name=file.js]
+</p>
