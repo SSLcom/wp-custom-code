d210a8b34188c66cf56f9530c28ae67294ef203d
Aaron Dancer <me@aarondancer.com>
2015-06-08 12:35:51 -0500

    Browse bundle folders

--- fragments/browse_bundle.php
+++ fragments/browse_bundle.php
@@ -9,13 +9,26 @@
 	</thead>
 	<tbody>
 		<?php
-			if ($handle = opendir(correctpath($root . '/bundle/' . $_GET['bundle']))) {
+			$prepath = $root . '/bundle/' . $_GET['bundle'];
+			$presub = "";
+			$i = 0;
+			while (isset($_GET['sub'.$i])) {
+				$prepath .= '/' . $_GET['sub' . $i];
+				$presub .= '&sub' . $i . '=' . $_GET['sub'.$i];
+				$i++;
+			}
+			$path = correctpath($prepath);
+			if ($handle = opendir($path)) {
 			    while (false !== ($entry = readdir($handle))) {
 			        if ($entry != "." && $entry != ".." && $entry !== "default." . $active_tab) {
 			        	echo "<tr>";
 			            echo "<td>$entry</td>";
-			            echo '<td style="text-align: right;"><a href="?page=sslcom_cc&tab=' . $active_tab . '&bundle=' . $entry . '">Edit</a> ';
-			            echo '<a href="?page=sslcom_cc&tab=' . $active_tab . '&delete=' . $entry . '">Delete</a></td>';
+			            echo '<td style="text-align: right;">';
+			            $presub .= '&sub' . $i . '=' . $entry;
+			            if (is_file(correctpath($path . "/" . $entry))) echo '<a href="?page=sslcom_cc&tab=' . $active_tab . '&bundle=' . $_GET['bundle'] . '&edit=' . $entry .  '">Edit</a> ';
+			            else echo '<a href="?page=sslcom_cc&tab=' . $active_tab . '&bundle=' . $_GET['bundle'] . $presub . '">Browse</a> ';
+			            echo '<a href="?page=sslcom_cc&tab=' . $active_tab . '&delete=' . $entry . '">Delete</a>';
+			            echo '</td>';
 			            echo '</tr>';
 			        }
 			    }
