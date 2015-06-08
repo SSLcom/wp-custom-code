253c0ad812a1f01530f058b8cc38851048856fba
Aaron Dancer <me@aarondancer.com>
2015-06-08 12:57:21 -0500

    Fixed bundles with contents not being deleted

--- fragments/bundle_handler.php
+++ fragments/bundle_handler.php
@@ -1,9 +1,21 @@
 <?php
+function rrmdir($dir) { 
+	if (is_dir($dir)) { 
+		$objects = scandir($dir); 
+		foreach ($objects as $object) { 
+			if ($object != "." && $object != "..") { 
+				if (filetype($dir."/".$object) == "dir") rrmdir($dir."/".$object); else unlink($dir."/".$object); 
+			} 
+		} 
+		reset($objects); 
+		rmdir($dir); 
+	} 
+} 
 $path = dirname(__FILE__);
 if (isset($_GET['delete'])) {
 	$folder = correctpath($root . "/bundle/" . $_GET['delete']);
 	if (is_dir($folder)) {
-		rmdir($folder);
+		rrmdir($folder);
 		include_once(correctpath($path . '/list_bundles.php'));
 	}
 }elseif (isset($_GET['bundle']) && $_GET['bundle'] !== '') {
