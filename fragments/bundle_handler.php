e15867b0d05c2adb616c25256974f4a7db5af997
Aaron Dancer <me@aarondancer.com>
2015-06-08 13:47:36 -0500

    Cleaned up code and added notice for when folder is deleted

--- fragments/bundle_handler.php
+++ fragments/bundle_handler.php
@@ -2,11 +2,12 @@
 function rrmdir($dir) { 
 	if (is_dir($dir)) { 
 		$objects = scandir($dir); 
-		foreach ($objects as $object) { 
-			if ($object != "." && $object != "..") { 
-				if (filetype($dir."/".$object) == "dir") rrmdir($dir."/".$object); else unlink($dir."/".$object); 
-			} 
-		} 
+		foreach ($objects as $object) {
+			if (($object != "." && $object != "..")
+				&& filetype($dir."/".$object) == "dir")
+				rrmdir($dir."/".$object);
+			else unlink($dir."/".$object); 
+		}
 		reset($objects); 
 		rmdir($dir); 
 	} 
@@ -16,6 +17,7 @@ if (isset($_GET['delete'])) {
 	$folder = correctpath($root . "/bundle/" . $_GET['delete']);
 	if (is_dir($folder)) {
 		rrmdir($folder);
+		echo '<div class="updated"><p>$folder has been deleted</p></div>';
 		include_once(correctpath($path . '/list_bundles.php'));
 	}
 }elseif (isset($_GET['bundle']) && $_GET['bundle'] !== '') {
